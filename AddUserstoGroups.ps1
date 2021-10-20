15dc43d977cdb222373f9b2531196f64bac95c85
b0dhi <66236462+b0dhi-scripts@users.noreply.github.com>
2021-10-20 10:06:38 -0400

    Update AddUserstoGroups.ps1

--- AddUserstoGroups.ps1
+++ AddUserstoGroups.ps1
@@ -1,14 +1,14 @@
 ### Bulk add users to a group ###
 
 ### Path to list of groups
-$GroupList = Get-Content "C:\Users\Administrator\Desktop\powershell\addADUsersToGroups\Groups.txt"			#CHANGE THIS ADDRESS
+$GroupList = Get-Content "C:\path\to\Groups.txt"			#CHANGE THIS ADDRESS
 
 ### Iterate through list of groups
 ForEach ($GroupName in $GroupList)
 {
 
 	### For each group in the list, use this list of users
-	$UserList = Get-Content "C:\Users\Administrator\Desktop\powershell\addADUsersToGroups\$($GroupName).txt"	#CHANGE THIS ADDRESS
+	$UserList = Get-Content "C:\path\to\$($GroupName).txt"	#CHANGE THIS ADDRESS
 	$Members = Get-ADGroupMember -Identity $GroupName -Recursive | Select -ExpandProperty SAMAccountName
  
 	### For each user, check if it's already in that group. If not, then add them.
